974288865cd7776b35aea2e8a5e46fcca56ba84c
Hung9n3 <chovameo352@gmail.com>
2022-11-21 14:40:04 +0700

    first commit

---
+++ WorkAlert/Function1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace WorkAlert
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            string defaultConnection = Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection");
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(defaultConnection);
+            var users = await response.Content.ReadFromJsonAsync<User>();
+            foreach(var w in users.Calendar.Works)
+            {
+                log.LogInformation($" Hello {users.Name} you have a work start at: {w.Start}");
+            }
+            
+        }
+    }
+}
---
+++ WorkAlert/Work.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkAlert
+{
+    public class Work 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Description { get; set; } = "";
+        public string Color { get; set; } = "";
+        public int CalendarId { get; set; }
+    }
+}
